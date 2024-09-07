ecc8509a4d1af1177bcb1f7a37f8931de0537f4f
srivastavarahul442 <srivastavarahul442@gmail.com>
2024-09-08 03:26:46 +0530

    adding file for Async Await

---
+++ asyncAwait.js
@@ -0,0 +1,69 @@
+/**
+ * async
+ * await
+ * async and await work behind the scense
+ * example
+ * error handling
+ * interview
+ * async/await vs promise.then/.catch
+ */
+
+
+//------------async-------------
+// const p = new Promise((resolve,reject)=>{
+//     resolve("Don")
+// })
+
+// // This always return promise
+// async function getData() {
+//     // return new Error("error");
+//     return p;
+// }
+
+// const promise = getData()
+// .then(function(data){
+//     console.log(data)
+// })
+
+
+//---------------------await---------------------
+
+// const p1 = new Promise((resolve,reject)=>{
+//     resolve("Mithu")
+// })
+
+
+// using await 
+// async function getData(){
+//     const promise = await p1;
+//     console.log(promise)
+// }
+
+// by using .then keyword
+// function getData(){
+//     p1.then(function(data){
+//         console.log(data)
+//     })
+// }
+
+// getData();
+
+//--------------how async and await works behind the scenes-----------
+
+const p = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("Promise resolved value")
+    },10000)
+})
+
+async function getData(){
+    console.log("Nameste javaScript")
+    const val1 = await p;
+    console.log("Nameste javaScript")
+    console.log(val1)
+
+    const val2 = await p;
+    console.log("Nameste javaScript")
+    console.log(val2)
+}
+getData()
